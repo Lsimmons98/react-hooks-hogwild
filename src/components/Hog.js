c1f98bfb0dfdc07e3e596c7049cf8aaad175c54f
Liam Simmons <liam.simmons@powerhrg.com>
2024-06-14 11:57:02 -0400

    fixed styling

--- src/components/Hog.js
+++ src/components/Hog.js
@@ -17,15 +17,17 @@ if(hog!==selectedHog){
     </div>
 	)} else {
     return(
-      <div id={hog.name} className='pigTile, ui eight wide column' onClick={handleClick}>
-      <h3>{hog.name}</h3>
-      <div>
-        <p>Greased: {hog.greased ? 'Yes' : 'No'}</p>
-        <p>Specialty: {hog.specialty}</p>
-        <p>Weight: {hog.weight}</p>
+      <div className='pigTile'>
+        <div id={hog.name} className='ui eight wide column' onClick={handleClick}>
+        <h3>{hog.name}</h3>
+        <div>
+          <p>Greased: {hog.greased ? 'Yes' : 'No'}</p>
+          <p>Specialty: {hog.specialty}</p>
+          <p>Weight: {hog.weight}</p>
+        </div>
+        <img className='pigImg' src={hog.image}/>
+        </div>
       </div>
-      <img className='pigImg' src={hog.image}/>
-		</div>
     )
   }
 };
