db51c37ac8a1e8adfebd70cadc3fb0a0af201b31
Liam Simmons <liam.simmons@powerhrg.com>
2024-06-14 11:54:52 -0400

    Fix tile alignment

--- src/components/Hog.js
+++ src/components/Hog.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Hog = ({hog, setSelectedHog, selectedHog}) => {
+
   const handleClick = (e) => {
     e.preventDefault()
     setSelectedHog(selectedHog===hog ? '' : hog)
@@ -8,20 +9,22 @@ const Hog = ({hog, setSelectedHog, selectedHog}) => {
 
 if(hog!==selectedHog){
 	return (
-		<div id={hog.name} className='pigTile' onClick={handleClick}>
-      <h3>{hog.name}</h3>
-      <img src={hog.image}/>
-		</div>
+    <div className='pigTile'>
+		  <div id={hog.name} className='ui eight wide column' onClick={handleClick}>
+        <h3>{hog.name}</h3>
+        <img className='pigImg' src={hog.image}/>
+		  </div>
+    </div>
 	)} else {
     return(
-      <div id={hog.name} className='pigTile' onClick={handleClick}>
+      <div id={hog.name} className='pigTile, ui eight wide column' onClick={handleClick}>
       <h3>{hog.name}</h3>
       <div>
         <p>Greased: {hog.greased ? 'Yes' : 'No'}</p>
         <p>Specialty: {hog.specialty}</p>
         <p>Weight: {hog.weight}</p>
       </div>
-      <img src={hog.image}/>
+      <img className='pigImg' src={hog.image}/>
 		</div>
     )
   }
